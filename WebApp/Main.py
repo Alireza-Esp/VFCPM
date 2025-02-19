22e0489f8f8a575f7c8a8c6b497a2227780ee882
Alireza Esmaeilpour <Alireza.esp07@gmail.com>
2025-02-19 20:59:40 +0330

    Webapp - update 1

--- WebApp/Main.py
+++ WebApp/Main.py
@@ -6,7 +6,6 @@ from Function import *
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
-
 st.markdown(sidebar_css, unsafe_allow_html=True)
 st.sidebar.markdown(
     f'<div style="text-align: justify;">{sidebar_text1}</div>', unsafe_allow_html=True)
@@ -15,15 +14,48 @@ st.sidebar.text(sidebar_text2)
 
 st.header("Welcome To VFCP Webapp!")
 st.markdown(
-    f'<div style="text-align: justify;">{intro_text}</div>', unsafe_allow_html=True)
+    f'<div style="text-align: justify;">{welcome_text}</div>', unsafe_allow_html=True)
 st.container(height=3, border=False)
 st.divider()
 
+st.header("How To Use VFCP Model?")
+st.markdown(
+    f'<div style="text-align: justify;">{help_text}</div>', unsafe_allow_html=True)
+st.divider()
+
+st.header("Prediction With Model")
 model_fuel = load_fuel_model(fuel_model_url, "model-fuel.pkl")
 model_CO2 = load_CO2_model(CO2_model_url, "model-CO2.pkl")
 encoder = load_encoder(encoder_url, "encoder.pkl")
 standardizer = load_standardizer(standardizer_url, "standardizer.pkl")
-
-st.header("How To Use VFCP Model?")
-
-st.header("Prediction With Model")
+st.markdown(
+    f'<div style="text-align: justify;">{prediction_text}</div>', unsafe_allow_html=True)
+st.container(border=False, height=1)
+number_of_cylinders = st.select_slider("Number Of Cylinders",
+                                       options=range(1,25),
+                                       value=4)
+engine_size = st.number_input("Engine Size (cc)",
+                              max_value=14000,
+                              min_value=500,
+                              step=1,
+                              value=2400)
+fuel_type = st.segmented_control("Fuel Type",
+                     options=fuel_type_list,
+                     )
+transmission_type = st.radio("Transmission Type",
+                     options=transmission_type_list,
+                     )
+number_of_gears = st.number_input("Number Of Gears",
+                                  min_value=1,
+                                  max_value=12,
+                                  step=1,
+                                  value=6)
+vehicle_class = st.selectbox("Vehicle Class",
+                                     options=vehicle_class_list)
+model_year = st.number_input("Model Year",
+                              max_value=2030,
+                              min_value=1980,
+                              step=1,
+                              value=2025)
+manufacturer = st.selectbox("Manufacturer",
+                            options=manufacturer_list)
--- WebApp/Text.py
+++ WebApp/Text.py
@@ -1,3 +1,25 @@
+import html
+shif_enter = html.unescape("<br />")
+
+fuel_type_list = ["Regular gasoline", "Premium gasoline", "Diesel", "Ethanol (E85)",
+                  "Natural Gas"]
+
+transmission_type_list = ["Manual (M)", "Automatic (A)", "Automated Manual (AM)",
+                          "Automatic With Select Shift (AS)", "Continuously Variable (AV)"]
+
+vehicle_class_list = ["MiniCompact", "SubCompact", "Compact", "Mid-Size", "Full-Size",
+                      "Station Wagon", "Pickup Truck", "MiniVan", "Van", "Sport Utility Vehicle (SUV)",
+                      "Two-Seater", "Special Purpose Vehicle"]
+
+manufacturer_list = ['Chevrolet', 'Ford', 'BMW', 'GMC', 'Mercedes-Benz', 'Toyota', 'Dodge',
+                     'Porsche', 'Audi', 'Volkswagen', 'Nissan', 'Mazda', 'Honda', 'Hyundai', 'Jeep', 'Subaru',
+                     'Volvo', 'Kia', 'Lexus', 'Cadillac', 'Chrysler', 'MINI', 'Jaguar', 'Pontiac', 'Buick',
+                     'Mitsubishi', 'Acura', 'Infiniti', 'Land Rover', 'Suzuki', 'Lincoln', 'Saab', 'Saturn',
+                     'Bentley', 'Ram', 'Maserati', 'Rolls-Royce', 'Lamborghini', 'Aston Martin', 'Ferrari',
+                     'Plymouth', 'FIAT', 'Oldsmobile', 'Genesis', 'Alfa Romeo', 'Mercury', 'Isuzu', 'Scion',
+                     'Eagle', 'Daewoo', 'smart', 'Hummer', 'Geo', 'Bugatti',]
+
+
 fuel_model_url = "https://github.com/Alireza-Esp/VFCPM/raw/refs/heads/main/Model%20&%20Objects/model-fuel.pkl"
 
 CO2_model_url = "https://github.com/Alireza-Esp/VFCPM/raw/refs/heads/main/Model%20&%20Objects/model-CO2.pkl"
@@ -6,8 +28,12 @@ encoder_url = "https://github.com/Alireza-Esp/VFCPM/raw/refs/heads/main/Model%20
 
 standardizer_url = "https://github.com/Alireza-Esp/VFCPM/raw/refs/heads/main/Model%20&%20Objects/standardizer.pkl"
 
-intro_text = """
-Whats the meaninig of VFCP? VFCP stands for "Vehicle Fuel Consumption Predictor". this is a Machine Learning Model that predict the rate of fuel consumtion and CO2 emission of a vehcile with respect to it's some characteristics like "Engine Size", "Number of Cylinders", "Fuel Type" and ... . Detialed informations of the project are explained in references. Are you excited? Let's use the model!
+welcome_text = """
+Whats the meaninig of VFCP? VFCP stands for "Vehicle Fuel Consumption Predictor".\
+this is a Machine Learning Model that predict the rate of fuel consumption and \
+CO2 emission of a vehcile with respect to it's some characteristics like "Engine Size", \
+"Number of Cylinders", "Fuel Type" and ... . Detialed informations of the project \
+are explained in references. Are you excited? Let's use the model!
 """
 
 sidebar_css = """
@@ -20,12 +46,37 @@ sidebar_css = """
 """
 
 sidebar_text1 = """
-🔹 This project has been defined and carried out as the "final undergraduate project".
+🔹 This project has been defined as the "Final Undergraduate Project" and carried out  by:
 """
 
 sidebar_text2 = """👨‍🎓 Alireza Esmaeilpour Peynavandi
+👨‍🏫 Supervising Professor: Dr. Alyari
 ⚙️ Field of Study:\n\t   Mechanical Engineernig - Automotive Subfield
 🏬 University:\n\t   Shahid Rajaee Teacher Training University
 📧 Email:\n\t   Alireza.esp07@gmail.com
 🌐 Website: \n\t   Alireza-Esp.ir
-🔗 GitHub:\n\t   Github.com/Alireza-Esp"""
+🔗 GitHub:\n\t   Github.com/Alireza-Esp\n\n\n
+Semester 8 - Winter 1403"""
+
+help_text = f"""Well, the VFCP is very easy to use! as previously satated, the model has \
+some features; that listed below:{shif_enter}
+▪️ Number Of Cylinders{shif_enter}
+▪️ Engine Size{shif_enter}
+▪️ Fuel Type{shif_enter}
+▪️ Transmission Type{shif_enter}
+▪️ Number Of Gears{shif_enter}
+▪️ Vehicle Class{shif_enter}
+▪️ Model Year{shif_enter}
+▪️ Manufacturer{shif_enter}
+In fact, this features is related to the vhicles that the model has trained from; and \
+if you want to make a prediction with model, you should pass this features to the model \
+and see that it predicts the "Combined Fuel Consumption" and "CO2 Emission" of your intended sample.{shif_enter}\
+However, there are some considerations that you should keep in mind. For example it's \
+better to enter interval [1995,2025] for "Model Year" feature however that the model \
+can make a prediction even if the values be outside of the interval. Or it's good \
+enter standard values for "Number Of Cylinders" featur; you know that probably we haven't \
+"11 Cylinders" vehicle.
+"""
+
+prediction_text = """Enter your intended features for prediction...
+"""
