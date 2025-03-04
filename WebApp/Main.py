30a1901f842e69cb8179d23e09dd80257d12626d
Alireza Esmaeilpour <Alireza.esp07@gmail.com>
2025-03-04 20:07:37 +0330

    Add 2025 dataset

--- WebApp/Main.py
+++ WebApp/Main.py
@@ -34,7 +34,7 @@ st.markdown(
     f'<div style="text-align: justify;">{prediction_text}</div>', unsafe_allow_html=True)
 st.container(border=False, height=1)
 number_of_cylinders = st.select_slider("Number Of Cylinders",
-                                       options=range(1,25),
+                                       options=range(1, 25),
                                        value=4)
 engine_size = st.number_input("Engine Size (cc)",
                               max_value=14000,
@@ -42,23 +42,23 @@ engine_size = st.number_input("Engine Size (cc)",
                               step=1,
                               value=2400)
 fuel_type = st.segmented_control("Fuel Type",
-                     options=fuel_type_list,
-                     )
+                                 options=fuel_type_list,
+                                 )
 transmission_type = st.radio("Transmission Type",
-                     options=transmission_type_list,
-                     )
+                             options=transmission_type_list,
+                             )
 number_of_gears = st.number_input("Number Of Gears",
                                   min_value=1,
                                   max_value=12,
                                   step=1,
                                   value=6)
 vehicle_class = st.selectbox("Vehicle Class",
-                                     options=vehicle_class_list)
+                             options=vehicle_class_list)
 model_year = st.number_input("Model Year",
-                              max_value=2030,
-                              min_value=1980,
-                              step=1,
-                              value=2025)
+                             max_value=2030,
+                             min_value=1980,
+                             step=1,
+                             value=2025)
 manufacturer = st.selectbox("Manufacturer",
                             options=manufacturer_list)
 
@@ -67,21 +67,22 @@ a1 = st.button("submit")
 result = submit_clicked()
 
 if result:
-    query_x_cat_ENCODED = pd.DataFrame(encoder.transform([[manufacturer, vehicle_class, transmission_type, fuel_type]]).toarray())
+    query_x_cat_ENCODED = pd.DataFrame(encoder.transform(
+        [[manufacturer, vehicle_class, transmission_type, fuel_type]]).toarray())
     query_x_cat_ENCODED.columns = query_x_cat_ENCODED.columns.astype(str)
-    query_x_num = pd.DataFrame({"Model year":[model_year], "Engine size (L)":[engine_size],
-                                "Cylinders":[number_of_cylinders], "Number of gears":[number_of_gears]})
+    query_x_num = pd.DataFrame({"Model year": [model_year], "Engine size (L)": [engine_size/1000],
+                                "Cylinders": [number_of_cylinders], "Number of gears": [number_of_gears]})
     query_x = pd.concat([query_x_cat_ENCODED, query_x_num], axis=1)
     query_x_ENCODED_STANDARDED = standardizer.transform(query_x)
 
-    st.dataframe(query_x_cat_ENCODED)
-    st.dataframe(query_x_num)
-    st.dataframe(query_x)
-    st.dataframe(query_x_ENCODED_STANDARDED)
+    # st.dataframe(query_x_cat_ENCODED)
+    # st.dataframe(query_x_num)
+    # st.dataframe(query_x)
+    # st.dataframe(query_x_ENCODED_STANDARDED)
 
     query_predicted_fuel = model_fuel.predict(query_x_ENCODED_STANDARDED)[0][0]
     query_predicted_CO2 = model_CO2.predict(query_x_ENCODED_STANDARDED)[0][0]
-    
+
     result = False
 
 st.text(query_predicted_fuel)
